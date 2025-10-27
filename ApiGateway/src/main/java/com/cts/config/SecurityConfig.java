2b811e0e9ff51881dcda667c76d87a49d87b2fb6
Pavan <214g1a3371@srit.ac.in>
2025-10-27 22:05:08 +0530

    added security

---
+++ ApiGateway/src/main/java/com/cts/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.cts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+	SecurityWebFilterChain secConfig(ServerHttpSecurity http) {
+		
+		return http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.authorizeExchange(exchanges->exchanges
+						.pathMatchers().permitAll())			
+				.build();
+		
+	}
+}
